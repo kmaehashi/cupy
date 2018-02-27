055a7c721a2a2e19582c9f76df32d6d4678f8eea
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-02-27 18:39:54 +0900

    wip

--- appveyor/install-cuda.ps1
+++ appveyor/install-cuda.ps1
@@ -45,12 +45,16 @@ function main () {
     $url = "https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows"
     $filepath = Download $filename $url
     RunCommand $filepath "-s"
+    $cuda_path = "$Env:ProgramFiles\NVIDIA GPU Computing Toolkit\CUDA\v9.1"
 
     # Download & Install cuDNN
     $filename = "cudnn-9.1-windows7-x64-v7.zip"
     $url = "http://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/$filename"
     $filepath = Download $filename $url
     Unzip $filepath $pwd.Path
+    Move-Item "cuda\bin\cudnn64_7.dll" "$cuda_path\bin"
+    Move-Item "cuda\lib\x64\cudnn.lib" "$cuda_path\lib\x64"
+    Move-Item "cuda\include\cudnn.h"   "$cuda_path\include"
 }
 
 main
