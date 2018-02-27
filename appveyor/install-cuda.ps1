202fd6963f9223c0bedd583afa77aab6808090c6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-02-27 20:27:39 +0900

    wip

--- appveyor/install-cuda.ps1
+++ appveyor/install-cuda.ps1
@@ -34,27 +34,50 @@ function RunCommand ($command, $command_args) {
     Start-Process -FilePath $command -ArgumentList $command_args -Wait -Passthru
 }
 
-function Unzip ([string]$zipfile, [string]$outpath) {
+function Unzip ($zipfile, $outpath) {
     Add-Type -AssemblyName System.IO.Compression.FileSystem
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
 function main () {
+    $CUDA_VERSION = "9.1"
+    $CUDA_RELEASE = "9.1.85"
+    $CUDNN_VERSION = "7"
+    $CUDNN_RELEASE = "7.0.5"
+    $CUDA_PATH = "${Env:ProgramFiles}\NVIDIA GPU Computing Toolkit\CUDA\v${CUDA_VERSION}"
+
     # Download & Install CUDA
-    $filename = "cuda_9.1.85_windows.exe"
-    $url = "https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows"
+    $filename = "cuda_${CUDA_RELEASE}_windows.exe"
+    $url = "https://developer.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/local_installers/cuda_${CUDA_RELEASE}_windows"
     $filepath = Download $filename $url
-    RunCommand $filepath "-s"
-    $cuda_path = "$Env:ProgramFiles\NVIDIA GPU Computing Toolkit\CUDA\v9.1"
+    RunCommand $filepath @"
+        -s
+        Display.Driver
+        nvcc_$CUDA_VERSION
+        visual_studio_integration_$CUDA_VERSION
+        cublas_$CUDA_VERSION
+        cublas_dev_$CUDA_VERSION
+        cudart_$CUDA_VERSION
+        cufft_$CUDA_VERSION
+        cufft_dev_$CUDA_VERSION
+        curand_$CUDA_VERSION
+        curand_dev_$CUDA_VERSION
+        cusolver_$CUDA_VERSION
+        cusolver_dev_$CUDA_VERSION
+        cusparse_$CUDA_VERSION
+        cusparse_dev_$CUDA_VERSION
+        nvrtc_$CUDA_VERSION
+        nvrtc_dev_$CUDA_VERSION
+    "@
 
     # Download & Install cuDNN
-    $filename = "cudnn-9.1-windows7-x64-v7.zip"
-    $url = "http://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/$filename"
+    $filename = "cudnn-${CUDA_VERSION}-windows7-x64-v${CUDNN_VERSION}.zip"
+    $url = "http://developer.download.nvidia.com/compute/redist/cudnn/v${CUDNN_RELEASE}/$filename"
     $filepath = Download $filename $url
     Unzip $filepath $pwd.Path
-    Move-Item "cuda\bin\cudnn64_7.dll" "$cuda_path\bin"
-    Move-Item "cuda\lib\x64\cudnn.lib" "$cuda_path\lib\x64"
-    Move-Item "cuda\include\cudnn.h"   "$cuda_path\include"
+    Move-Item "cuda\bin\cudnn64_${CUDNN_VERSION}.dll"   "${CUDA_PATH}\bin"
+    Move-Item "cuda\lib\x64\cudnn.lib"                  "${CUDA_PATH}\lib\x64"
+    Move-Item "cuda\include\cudnn.h"                    "${CUDA_PATH}\include"
 }
 
 main
